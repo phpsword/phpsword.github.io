2869069c79b2da34aa329185c2cd5e4aaefb6403
williarin <69433384+williarin@users.noreply.github.com>
2022-09-18 10:52:05 +0000

    Deploying to publish from @ phpsword/phpsword.github.io@9d87063d5497bd374a31a4c1c10b046e0febdbdb 🚀

---
+++ assets/index.html.dc0c3056.js
@@ -0,0 +1,22 @@
+import{_ as a,r,o as d,c as l,b as e,d as n,w as s,e as t,a as c}from"./app.d5a6fc4c.js";const h={},u=e("h1",{id:"what-is-sword",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#what-is-sword","aria-hidden":"true"},"#"),t(" What is Sword?")],-1),p=t("Modern WordPress development has been around for a while with software like "),_={href:"https://roots.io/bedrock/",target:"_blank",rel:"noopener noreferrer"},g=t("Bedrock"),f=t("."),m=e("p",null,"Sword takes an alternative approach. Instead of being WordPress with extra features and new file structure, it's Symfony embedding WordPress. All WordPress pages and logic, including WP-CLI, run through Symfony.",-1),w=e("p",null,"Welcome in a new era.",-1),y=e("h2",{id:"get-started",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#get-started","aria-hidden":"true"},"#"),t(" Get started")],-1),b=t("Please see "),x=t("installation"),P=t(" page for instructions on creating a new project."),W=e("h2",{id:"features",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#features","aria-hidden":"true"},"#"),t(" Features")],-1),v=c("<li>PHP &gt;= 8.1 (latest version for new projects)</li><li>Symfony &gt;= 6.1 (latest version for new projects)</li><li>WordPress &gt;= 5.6 (latest version for new projects)</li><li>WordPress files hidden from the web server, one <code>/public/index.php</code> entrypoint</li><li>Bcrypt password hashing by default, without plugin</li><li><strong>Composer-based</strong> WordPress updates and vendor dependencies</li><li><strong>Dependency injection</strong> for WordPress code</li><li><strong>Synchronized auth</strong> between WordPress and Symfony</li><li><strong>Compatible</strong> with most WordPress <strong>themes</strong> and <strong>plugins</strong></li><li>Possibility to <strong>use both Symfony and WordPress assets</strong> in your theme</li>",10),k=t("Docker"),S=t(" development and production ready environments"),j=t("WP-CLI"),C=t(" as Symfony command"),L=e("h2",{id:"project-structure",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#project-structure","aria-hidden":"true"},"#"),t(" Project structure")],-1),B=e("p",null,[t("The project structure is the same as a standard Symfony project, with a new "),e("code",null,"wp"),t(" folder containing WordPress core, theme, plugins and uploads.")],-1),I=e("div",{class:"language-text ext-text"},[e("pre",{class:"language-text"},[e("code",null,`\u251C\u2500\u2500 assets
+\u251C\u2500\u2500 bin
+\u251C\u2500\u2500 config
+\u251C\u2500\u2500 migrations
+\u251C\u2500\u2500 public
+\u2502  \u2514\u2500\u2500 index.php
+\u251C\u2500\u2500 src
+\u2502  \u251C\u2500\u2500 Controller
+\u2502  \u251C\u2500\u2500 Entity
+\u2502  \u2514\u2500\u2500 Repository
+\u251C\u2500\u2500 templates
+\u251C\u2500\u2500 tests
+\u251C\u2500\u2500 translations
+\u251C\u2500\u2500 var
+\u2514\u2500\u2500 wp
+   \u251C\u2500\u2500 content
+   \u2502  \u251C\u2500\u2500 plugins
+   \u2502  \u251C\u2500\u2500 themes
+   \u2502  \u2514\u2500\u2500 uploads
+   \u251C\u2500\u2500 core
+   \u2514\u2500\u2500 wp-config.php
+`)])],-1);function E(N,V){const i=r("ExternalLinkIcon"),o=r("RouterLink");return d(),l("div",null,[u,e("p",null,[p,e("a",_,[g,n(i)]),f]),m,w,y,e("p",null,[b,n(o,{to:"/guide/installation.html"},{default:s(()=>[x]),_:1}),P]),W,e("ul",null,[v,e("li",null,[e("strong",null,[n(o,{to:"/guide/tooling.html#php-image"},{default:s(()=>[k]),_:1})]),S]),e("li",null,[e("strong",null,[n(o,{to:"/guide/tooling.html#wp-cli"},{default:s(()=>[j]),_:1})]),C])]),L,B,I])}const D=a(h,[["render",E],["__file","index.html.vue"]]);export{D as default};
